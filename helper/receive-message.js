092e721da996ecf1a72b88bf5602c0f2d6fd6097
tnguyen452 <tnntech@gatech.edu>
2018-05-08 13:59:38 -0400

    debug

--- helper/receive-message.js
+++ helper/receive-message.js
@@ -2,13 +2,12 @@ const bodyParser = require('body-parser');
 const request = require('request');
 const sendResponse = require('./send_respond.js');
 
-function handleMessage(sender_psid, received_message) => {
+function handleMessage(sender_psid, received_message) {
 	console.log('test')
 	let key = received_message.text;
 	sendResponse.directMessage(sender_psid,key);
 }
 
 module.exports = {
-  handleMessage,
-  handleQuickReply
+  handleMessage
 };
